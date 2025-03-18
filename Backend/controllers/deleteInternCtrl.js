0161c45d93e2b177ab5873ac08efbd88d696003e
Lisandro Jimenez <jimenezlisandro88@gmail.com>
2025-03-18 14:00:02 -0700

    fix: modified code to use with a mysql query

--- Backend/controllers/deleteInternCtrl.js
+++ Backend/controllers/deleteInternCtrl.js
@@ -1,27 +1,26 @@
 import promisePool from '../config/database.js';
 
 const deleteInternCtrl = {
-    deleteIntern: async(req,res)=>{
-        try{
+    deleteIntern: async(req, res) => {
+        try {
             const { internID } = req.params;
-            if(!internID) {
-                return res.status(400).json({message: 'Intern ID is required'});
+            if (!internID) {
+                return res.status(400).json({ message: 'Intern ID is required' });
             }
 
             const deleteInternQuery = `
-            DELETE FROM bizznestflow2.interns WHERE InternID = ?`;
+                DELETE FROM bizznestflow2.interns WHERE InternID = ?`;
             const [result] = await promisePool.execute(deleteInternQuery, [internID]);
 
-            if(result.affectedRows === 0){
-                return res.status(400).json({message : 'Intern not found'})
+            if (result.affectedRows === 0) {
+                return res.status(400).json({ message: 'Intern not found' });
             }
-            res.status(200).json({message: "Intern deleted successfully"})
-        }catch(error){ 
-            console.error('Error deleting intern', error.message);
-            res.status(500).json({message : 'Error deleting Intern'})
-
+            res.status(200).json({ message: "Intern deleted successfully" });
+        } catch (error) {
+            console.error('Error deleting intern:', error.message);
+            res.status(500).json({ message: 'Error deleting intern' });
         }
     },
-}
+};
 
 export default deleteInternCtrl;
