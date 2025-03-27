dcc9fb2c7ee69507751496a73eadba09f672d48d
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-26 20:01:37 -0700

    Update generate_conf.sh

--- Proxy/generate_conf.sh
+++ Proxy/generate_conf.sh
@@ -3,15 +3,17 @@ cat <<EOF > nginx.conf
 user  nginx;
 worker_processes  auto;
 
-server {
-    listen 80;
-    server_name localhost;
-
-    location / {
-        proxy_pass http://backend-development:3000;
-        proxy_set_header Host $host;
-        proxy_set_header X-Real-IP $remote_addr;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+http {
+    server {
+        listen 80;
+        server_name localhost;
+    
+        location / {
+            proxy_pass http://backend-development:3000;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        }
     }
 }
 EOF
