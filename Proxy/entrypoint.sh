42d0d8b890c21890feebf55353de42edf9a35489
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-28 11:30:24 -0700

    Update entrypoint.sh

--- Proxy/entrypoint.sh
+++ Proxy/entrypoint.sh
@@ -3,18 +3,18 @@
 nginx &
 
 # Wait for Nginx to be fully up
-until curl --silent --fail http://localhost:5555/health; do
+until curl --silent --fail http://localhost/basePage; do
   echo "Waiting for Nginx to be up..."
   sleep 1
 done
 
-# Obtain SSL certificate using Certbot
+Obtain SSL certificate using Certbot
 
-#echo "Obtaining SSL certificate for $DOMAIN_NAME."
-#certbot --nginx --non-interactive --agree-tos -d $DOMAIN_NAME --redirect || { echo "Certbot failed"; tail -n 50 /var/log/letsencrypt/letsencrypt.log; exit 1; }
+echo "Obtaining SSL certificate for $DOMAIN_NAME."
+certbot --nginx --non-interactive --agree-tos -d $DOMAIN_NAME --redirect || { echo "Certbot failed"; tail -n 50 /var/log/letsencrypt/letsencrypt.log; exit 1; }
 
-# Set up automatic renewal (ensure cron is running or use another approach)
-#echo "0 12 * * * root certbot renew --quiet && nginx -s reload" >> /etc/crontab
+Set up automatic renewal (ensure cron is running or use another approach)
+echo "0 12 * * * root certbot renew --quiet && nginx -s reload" >> /etc/crontab
 
-# Keep the container running
+Keep the container running
 tail -f /var/log/nginx/access.log
