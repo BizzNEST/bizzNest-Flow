b8d045cdb478a64d769fb1a0071b40d4de31acf3
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-27 21:46:11 -0700

    Update generate_conf.sh

--- Proxy/generate_conf.sh
+++ Proxy/generate_conf.sh
@@ -2,9 +2,11 @@
 cat <<EOF > nginx.conf
 user  nginx;
 worker_processes  auto;
+
 events {
     worker_connections  1024;
-    }
+}
+
 http {
     server {
         listen 80;
@@ -13,16 +15,16 @@ http {
             proxy_pass http://backend-development:3000;
         }
     }
+
     server {
         listen 127.0.0.1:5555;
         
         location /health {
-            stub_status on;         # Enable status endpoint
-            access_log off;         # Disable logging for this endpoint
-            allow 127.0.0.1;        # Only allow localhost access
-            deny all;               # Deny all other connections
+            stub_status on;
+            access_log off;
+            allow 127.0.0.1;
+            deny all;
         }
     }
-}
 }
 EOF
