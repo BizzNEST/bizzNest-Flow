152121f6c00df22b02bff33dc581c7366abf6cbe
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-27 21:20:06 -0700

    Update entrypoint.sh

--- Proxy/entrypoint.sh
+++ Proxy/entrypoint.sh
@@ -3,7 +3,7 @@
 nginx &
 
 # Wait for Nginx to be fully up
-until curl --silent --fail http://localhost; do
+until curl --silent --fail http://backend-development:3000; do
   echo "Waiting for Nginx to be up..."
   sleep 1
 done
