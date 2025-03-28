c0f6d0fe7e1dd6e0165f89ef68a9bc06e0011515
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-27 21:28:29 -0700

    Update generate_conf.sh

--- Proxy/generate_conf.sh
+++ Proxy/generate_conf.sh
@@ -13,5 +13,16 @@ http {
             proxy_pass http://backend-development:3000;
         }
     }
+    server {
+        listen 127.0.0.1:5555;
+        
+        location /health {
+            stub_status on;         # Enable status endpoint
+            access_log off;         # Disable logging for this endpoint
+            allow 127.0.0.1;        # Only allow localhost access
+            deny all;               # Deny all other connections
+        }
+    }
+}
 }
 EOF
