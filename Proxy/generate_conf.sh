3c313a175868d8a7de150e0dd168aae7ce21c6be
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-28 11:14:08 -0700

    Update generate_conf.sh

--- Proxy/generate_conf.sh
+++ Proxy/generate_conf.sh
@@ -2,28 +2,61 @@
 cat <<EOF > nginx.conf
 user  nginx;
 worker_processes  auto;
+pid /var/run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
 
 events {
-    worker_connections  1024;
+    worker_connections 1024;
 }
 
 http {
+    server_tokens off;
+
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
+    keepalive_timeout 65;
+    types_hash_max_size 2048;
+
+    include /etc/nginx/mime.types;
+    default_type application/octet-stream;
+
+    # Logging
+    access_log /var/log/nginx/access.log;
+    error_log /var/log/nginx/error.log;
+
+    # Gzip settings
+    gzip on;
+    gzip_disable "msie6";
+
     server {
         listen 80;
-    
+        server_name $1 www.$1;
+
+        location /.well-known/acme-challenge/ {
+            root /var/www/html;
+        }
+
         location / {
-            proxy_pass http://backend-development:3000;
+            return 301 https://\$host\$request_uri;
         }
     }
 
     server {
-        listen 127.0.0.1:5555;
-        
-        location /health {
-            stub_status on;
-            access_log off;
-            allow 127.0.0.1;
-            deny all;
+        listen 443 ssl;
+        server_name $1 www.$1;
+
+        ssl_certificate /etc/letsencrypt/live/$1/fullchain.pem;
+        ssl_certificate_key /etc/letsencrypt/live/$1/privkey.pem;
+        include /etc/letsencrypt/options-ssl-nginx.conf;
+        ssl_dhparam /etc/letsencrypt/ssl-dhparams.pem;
+
+        location / {
+            proxy_pass http://$2-backend:3000;  # Replace 5000 with the port your backend service uses
+            proxy_set_header Host \$host;
+            proxy_set_header X-Real-IP \$remote_addr;
+            proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
+            proxy_set_header X-Forwarded-Proto \$scheme;
         }
     }
 }
