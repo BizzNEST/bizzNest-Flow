f29d7b18be091d25e08eb1849ae97584c452f145
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-28 12:45:02 -0700

    Update entrypoint.sh

--- Proxy/entrypoint.sh
+++ Proxy/entrypoint.sh
@@ -3,7 +3,7 @@
 nginx &
 
 # Wait for Nginx to be fully up
-until curl --silent --fail http://localhost/.well-known/acme-challenge/; do
+until curl --silent --fail http://localhost:5555/health; do
   echo "Waiting for Nginx to be up..."
   sleep 1
 done
