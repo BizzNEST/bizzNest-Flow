d5bf174d790ad15781e756d50adbf71d76b75963
Alan Buenrostro Cervantes <141185166+DruPro@users.noreply.github.com>
2025-03-28 16:47:31 -0700

    Update entrypoint.sh

--- Proxy/entrypoint.sh
+++ Proxy/entrypoint.sh
@@ -7,7 +7,7 @@ nginx &
 # Obtain SSL certificate using Certbot
 
 echo "Obtaining SSL certificate for $DOMAIN_NAME."
-certbot --webroot --non-interactive --agree-tos -d $DOMAIN_NAME --redirect || { echo "Certbot failed"; tail -n 50 /var/log/letsencrypt/letsencrypt.log; exit 1; }
+certbot certonly --webroot --non-interactive --agree-tos -d $DOMAIN_NAME --redirect || { echo "Certbot failed"; tail -n 50 /var/log/letsencrypt/letsencrypt.log; exit 1; }
 
 # Set up automatic renewal (ensure cron is running or use another approach)
 echo "0 12 * * * root certbot renew --quiet && nginx -s reload" >> /etc/crontab
